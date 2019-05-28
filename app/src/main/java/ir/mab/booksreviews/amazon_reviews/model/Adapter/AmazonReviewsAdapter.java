6dccecb25b169b3c21dd30d75075f8750ffdf831
mabahmani <aminbahmani10@gmail.com>
2019-05-28 20:12:05 +0430

    Add Reviews Expandable Text View.

--- app/src/main/java/ir/mab/booksreviews/amazon_reviews/model/Adapter/AmazonReviewsAdapter.java
+++ app/src/main/java/ir/mab/booksreviews/amazon_reviews/model/Adapter/AmazonReviewsAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,12 +30,29 @@ public class AmazonReviewsAdapter extends RecyclerView.Adapter<AmazonReviewsAdap
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AmazonReviewsAdapter.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final AmazonReviewsAdapter.ViewHolder viewHolder, int i) {
         viewHolder.userName.setText(amazonReviews.get(i).getName());
         viewHolder.title.setText(amazonReviews.get(i).getTitle());
         viewHolder.date.setText(amazonReviews.get(i).getDate());
         viewHolder.text.setText(amazonReviews.get(i).getText());
 
+        viewHolder.expand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewHolder.text.setMaxLines(Integer.MAX_VALUE);
+                viewHolder.expand.setVisibility(View.GONE);
+                viewHolder.collapse.setVisibility(View.VISIBLE);
+            }
+        });
+
+        viewHolder.collapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewHolder.text.setMaxLines(4);
+                viewHolder.expand.setVisibility(View.VISIBLE);
+                viewHolder.collapse.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -44,12 +62,15 @@ public class AmazonReviewsAdapter extends RecyclerView.Adapter<AmazonReviewsAdap
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView userName,title,date,text;
+        ImageView expand , collapse;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             userName = itemView.findViewById(R.id.user_name);
             title = itemView.findViewById(R.id.title);
             date = itemView.findViewById(R.id.date);
             text = itemView.findViewById(R.id.text);
+            expand = itemView.findViewById(R.id.expand_img);
+            collapse = itemView.findViewById(R.id.collapse_img);
         }
     }
 }
